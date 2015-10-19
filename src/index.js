11107f3333cbe61c67ad66ee2bf5f5272cef5ac4
Sergey Demyanov <demon-xxi@users.noreply.github.com>
2015-10-19 14:58:31 -0400

    decodeURIComponent cookie

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@
         for(var i=0; i<ca.length; i++) {
             var c = ca[i];
             while (c.charAt(0)==' ') c = c.substring(1);
-            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+            if (c.indexOf(name) == 0) return decodeURIComponent(c.substring(name.length,c.length));
         }
         return "";
     }    
